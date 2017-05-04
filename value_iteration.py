2ed26d1d8d6a5147b98d40d23b28eb3458e1a03b
James Fielder <jamesfielder@Jamess-MBP.attlocal.net>
2017-05-04 00:05:23 -0500

    loop

--- value_iteration.py
+++ value_iteration.py
@@ -15,19 +15,50 @@ def value_iteration(environment_file,non_terminal_reward,gamma,k_iterations):
 
     envo = []
     for c in content:
-        t = []
-        for v in c:
-            t.append(v)
+        t = c.split(",")
         envo.append(t)
     
     global X_MAX
     global Y_MAX
+    global states
 
-    X_MAX = len(c[0])
-    Y_MAX = len(c)
-    
+    envo.reverse()
+
+    X_MAX = len(envo)
+    Y_MAX = len(envo[0])
+    states = envo
+
+    n = X_MAX*Y_MAX
+    u_2 = [0.0]*n
+
+    for i in range(k_iterations):
+        u = u_2[:]
+        delta = 0
+
+        for s in states:
+            u_2 = 
+
+    print(envo)
+    print(u)
+
     p = transition((0,0),(0,0),"left")
-    print(p)
+
+def is_float(s):
+    try: 
+        float(s)
+        return True
+    except ValueError:
+        return False
+def reward(x,y):
+    val = states[x][y]
+    if val == ".":
+        return -0.04
+    else:
+        if is_float(val):
+            return float(val)
+        elif val == 'X':
+            return 0.0
+        
 
 def transition(dest,origin,action):
     dest_x = dest[0]
@@ -42,24 +73,24 @@ def transition(dest,origin,action):
     if dest_x == origin_x and dest_y == origin_y:
         #left
         if action == "left":
-            if (dest_x) == -1: p += .8
-            if (dest_y) == -1: p += .1
-            if (dest_y) == Y_MAX: p += .1 
+            if (origin_x - 1) == -1: p += .8
+            if (origin_y - 1) == -1: p += .1
+            if (origin_y + 1) == Y_MAX: p += .1 
         #up 
         if action == "up":
-            if (dest_y) == Y_MAX: p += .8
-            if (dest_x) == -1: p += .1
-            if (dest_x) == X_MAX: p += .1
+            if (origin_y + 1) == Y_MAX: p += .8
+            if (origin_x - 1) == -1: p += .1
+            if (origin_x + 1) == X_MAX: p += .1
         #right 
         if action == "right":
-            if (dest_x) == X_MAX: p += .8
-            if (dest_y) == Y_MAX: p += .1
-            if (dest_y) == -1: p+=.1
+            if (origin_x + 1) == X_MAX: p += .8
+            if (origin_y + 1) == Y_MAX: p += .1
+            if (origin_y - 1) == -1: p += .1
         #down 
         if action == "down":
-            if (dest_y) == -1: p+=.8
-            if (dest_x) == X_MAX: p+= .1
-            if (dest_x) == -1: p+= .1   
+            if (origin_y - 1) == -1: p+=.8
+            if (origin_x + 1) == X_MAX: p+= .1
+            if (origin_x - 1) == -1: p+= .1   
     #moving left
     if dest_x == origin_x - 1 and dest_y == origin_y:   
         if action == "left":
@@ -81,7 +112,7 @@ def transition(dest,origin,action):
             pass
     #moving right
     if dest_x == (origin_x + 1) and dest_y == origin_y:
-        if action == "left":pass
+        if action == "left": pass
         if action == "up": 
             if (dest_x) != X_MAX: p += .1
         if action == "right": 
